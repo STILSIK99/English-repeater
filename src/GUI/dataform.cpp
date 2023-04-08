4c29f0aa5ad6de7ff72fba74a7790ace63950dcb
STILSIK99 <viktorPUH@gmail.com>
2023-04-08 12:03:01 +0400

    Start developing database module

--- src/GUI/dataform.cpp
+++ src/GUI/dataform.cpp
@@ -10,3 +10,22 @@ DataForm::~DataForm()
 {
     deleteLater();
 }
+
+void DataForm::on_pushButton_clicked()
+{
+    ui->tableWidget->setRowCount (ui->tableWidget->rowCount() + 1);
+}
+
+
+void DataForm::on_pushButton_2_clicked()
+{
+    ui->tableWidget->setRowCount (ui->tableWidget->rowCount() - 1);
+}
+
+
+void DataForm::on_pushButton_3_clicked()
+{
+    //send to database
+
+}
+
